2a2436d3c75235ff907727925bfe5a168e5e9d64
Paweł Woźny <pawelpwozny@gmail.com>
2017-12-15 08:54:55 +0100

    canvas star example

--- frontend/examples/canvas-stars/canvas-stars.js
+++ frontend/examples/canvas-stars/canvas-stars.js
@@ -18,21 +18,23 @@ const canvasStars = () => {
   body.append(canvas);
 
   function clear(){
-    console.log('test');
     canvasContext.clearRect(0,0, areaWidth, areaHeight);
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0, areaWidth, areaHeight);
   }
 
-  function drawPoint(){
+  let it = 0;
+
+  function drawPoint(x, it){
     clear();
-    const x = (Math.round(Math.random() * 100) / 100) * areaWidth, y = (Math.round(Math.random() * 100) / 100) * areaHeight;
+    // const x = (Math.round(Math.random() * 100) / 100) * areaWidth, y = (Math.round(Math.random() * 100) / 100) * areaHeight;
+    const y = it + it*10;
     // canvasContext.fillStyle = 'white';
     canvasContext.strokeStyle = 'white';
     canvasContext.lineWidth = 1;
 
     // canvasContext.fillRect(x,y, 5, 5);
-    // canvasContext.beginPath();
+    canvasContext.beginPath();
     canvasContext.moveTo(x,y);
     canvasContext.lineTo(x+10, y);
     canvasContext.moveTo(x,y);
@@ -51,13 +53,29 @@ const canvasStars = () => {
     canvasContext.lineTo(x-8, y+8);
     canvasContext.moveTo(x,y);
     canvasContext.stroke();
-    // canvasContext.closePath();
-    requestAnimationFrame(drawPoint);
+    canvasContext.closePath();
+
+    it++;
+    // setTimeout(() => {drawPoint(x, it)}, 1000);
+    requestAnimationFrame(() => {drawPoint(x, it)});
   }
 
   clear();
+  // setInterval(() => {
+  //   let x = (Math.round(Math.random() * 100) / 100) * areaWidth,
+  //   it = 0;
+  //   drawPoint(x, it)
+  // }, 1000);
 
-  setTimeout(drawPoint, 1000);
+  function createFlake(){
+    let x = (Math.round(Math.random() * 100) / 100) * areaWidth,
+    it = 0;
+    drawPoint(x, it);
+    // setTimeout(createFlake, 300);
+    // 
+  }
+
+  createFlake();
 };
 
 export default canvasStars;
